e29e2e6ee6f26f01cef1e25dc3681421621c151c
p3chaeyeon <p3chaeyeon@gmail.com>
2024-10-15 16:51:53 +0900

    1

---
+++ src/main/java/com/spring/NCPSpringMaven/MainController.java
@@ -0,0 +1,18 @@
+// NCPSpringMaven/src/main/java/com/spring/NCPSpringMaven/MainController.java
+package com.spring.NCPSpringMaven;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value="/")
+	public String index() {
+		return "/index";
+	}
+	
+	
+	
+	
+}
